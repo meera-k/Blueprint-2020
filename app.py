8c338f49a9ef9b2830cafd2cdb6ab9ac637a283d
meera-k <45214387+meera-k@users.noreply.github.com>
2020-02-09 14:15:31 -0500

    created another collection

--- app.py
+++ app.py
@@ -20,9 +20,10 @@ def welcome_page():
 def next(page_num):
     # page_num+=1
     # x = "bruhhh"
-    data[int(page_num)]
+    render_template('quiz.html', page_num=page_num, question=question_dict[int(page_num)], answers=answer_dict[int(page_num)])
+    single_data[int(page_num)] = request.form['val']
     print (answer_dict[(int(page_num))])
-    return render_template('quiz.html', page_num=page_num, question=question_dict[int(page_num)], answers=answer_dict[int(page_num)])
+    return 'success'
     #return render_template('questionpage.html', question_dict[page_num], answer_dict[page_num])
 
 # @app.route('/birthday', methods=['POST'])
@@ -44,29 +45,29 @@ def birthday():
     render_template('birthdaypage.html')
     birthday = datetime.strptime(request.form['date'], '%m/%d/%y')
     if birthday < datetime(2020, 1, 20) or datetime(2020, 12, 23) < birthday:
-        data['zodiac'] = 'Capricorn'
+        single_data['zodiac'] = 'Capricorn'
     elif datetime(2020, 1, 21) < birthday < datetime(2020, 2, 19):
-        data['zodiac'] = 'Aquarius'
+        single_data['zodiac'] = 'Aquarius'
     elif datetime(2020, 2, 20) < birthday < datetime(2020, 3, 20):
-        data['zodiac'] = 'Pisces'
+        single_data['zodiac'] = 'Pisces'
     elif datetime(2020, 3, 21) < birthday < datetime(2020, 4, 20):
-        data['zodiac'] = 'Aries'
+        single_data['zodiac'] = 'Aries'
     elif datetime(2020, 4, 21) < birthday < datetime(2020, 5, 21):
-        data['zodiac'] = 'Taurus'
+        single_data['zodiac'] = 'Taurus'
     elif datetime(2020, 5, 22) < birthday < datetime(2020, 6, 21):
-        data['zodiac'] = 'Gemini'
+        single_data['zodiac'] = 'Gemini'
     elif datetime(2020, 6, 22) < birthday < datetime(2020, 7, 22):
-        data['zodiac'] = 'Cancer'
+        single_data['zodiac'] = 'Cancer'
     elif datetime(2020, 7, 23) < birthday < datetime(2020, 8, 21):
-        data['zodiac'] = 'Leo'
+        single_data['zodiac'] = 'Leo'
     elif datetime(2020, 8, 22) < birthday < datetime(2020, 9, 23):
-        data['zodiac'] = 'Virgo'
+        single_data['zodiac'] = 'Virgo'
     elif datetime(2020, 9, 24) < birthday < datetime(2020, 10, 23):
-        data['zodiac'] = 'Libra'
+        single_data['zodiac'] = 'Libra'
     elif datetime(2020, 10, 24) < birthday < datetime(2020, 11, 22):
-        data['zodiac'] = 'Scorpio'
+        single_data['zodiac'] = 'Scorpio'
     else: #if datetime(2020, 11, 23) < birthday < datetime(2020, 12, 22)
-        data['zodiac'] = 'Sagittarius'
+        single_data['zodiac'] = 'Sagittarius'
     return 'success'
     #TODO make sure that info is passed in in datetime format
     #add zodiac into cur_user doc as last entry
