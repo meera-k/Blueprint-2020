a4e7490ff3a89b418498693ad0a94542109c18e0
abriskin <45403282+abriskin@users.noreply.github.com>
2020-02-09 13:19:00 -0500

    alex help

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ client = MongoClient("mongodb+srv://admin:admin@cluster0-e5pzb.mongodb.net/test?
 db = client['Blueprint-2020'] #database - only one necessary
 zodiac_key = db['zodiac_key'] #collection - perfect zodiac key
 data = db['data'] #collection - actual user data - will hold single and total session data
-page_num = 0
+# page_num = 0
 question_dict = {1: "", 2: "", 3: ""}
 answer_dict = {1: ["smart", "dumb"], 2: ["smart", "dumb"], 3: ["smart", "dumb"]}
 
@@ -16,19 +16,26 @@ def welcome_page():
     return render_template('welcomepage.html') #returns html file sent to browser given the file and parameter for variables
     #find returns all elements that fit a certain condition (eg. 'ts': [certain date] )
 
-@app.route('/next', methods=['GET', 'POST'])
-def next():
-    page_num+=1
-    return render_template('questionpage.html', page_num)
+@app.route('/quiz/<page_num>', methods=['GET', 'POST'])
+def next(page_num):
+    # page_num+=1
+    # x = "bruhhh"
+    print (answer_dict[(int(page_num))])
+    return render_template('quiz.html', page_num=page_num, answers=answer_dict[int(page_num)])
     #return render_template('questionpage.html', question_dict[page_num], answer_dict[page_num])
 
-@app.route('/birthday', methods=['POST'])
-def birthday():
-    #TODO make sure that info is passed in in datetime format
-    #add zodiac into cur_user doc as last entry
+# @app.route('/birthday', methods=['POST'])
+# def birthday():
+#     #TODO make sure that info is passed in in datetime format
+#     #add zodiac into cur_user doc as last entry
 
-@app.route('/stats', methods=['GET'])
-def stats():
-    #add info to all_data doc in data collection
-    #do calculations
-    #clear out current user doc
+# @app.route('/stats', methods=['GET'])
+# def stats():
+#     #add info to all_data doc in data collection
+#     #do calculations
+#     #clear out current user doc
+
+# @app.route('/', defaults=('path': ''))
+# @app.route('/<path:path>'')
+
+app.run(port=3000, debug=True)
