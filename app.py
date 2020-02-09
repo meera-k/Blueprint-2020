100d1f3e3a594ac88f668e686300d0e683c4233c
meera-k <45214387+meera-k@users.noreply.github.com>
2020-02-09 17:04:16 -0500

    final maybe?

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ client = MongoClient("mongodb+srv://admin:admin@cluster0-e5pzb.mongodb.net/test?
 db = client['Blueprint-2020'] #database - only one necessary
 total_data = db['total_data'] #collection - actual user data - will hold single and total session data
 single_data = []
+signs_array = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"]
 
 @app.route('/', methods=['GET']) #specifies path and which methods it will work w
-def home():
+def home(): 
     return render_template('questions.html') #returns html file sent to browser given the file and parameter for variables
 
 # @app.route('/stats', methods=['GET'])
@@ -17,6 +18,47 @@ def home():
 #     #add info to all_data doc in data collection
 #     #do calculations
 #     #clear out current user doc
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    raw_single = request.form['raw_single']
+    for x in range(0, raw_single.length):
+        if raw_single[x] == "Aries":
+            single_data[0]+=1
+        elif raw_single[x] == "Taurus":
+            single_data[1]+=1
+        elif raw_single[x] == "Gemini":
+            single_data[2]+=1
+        elif raw_single[x] == "Cancer":
+            single_data[3]+=1
+        elif raw_single[x] == "Leo":
+            single_data[4]+=1
+        elif raw_single[x] == "Virgo":
+            single_data[5]+=1
+        elif raw_single[x] == "Libra":
+            single_data[6]+=1
+        elif raw_single[x] == "Scorpio":
+            single_data[7]+=1   
+        elif raw_single[x] == "Sagittarius":
+            single_data[8]+=1
+        elif raw_single[x] == "Capricorn":
+            single_data[9]+=1
+        elif raw_single[x] == "Aquarius":
+            single_data[10]+=1
+        elif raw_single[x] == "Pisces":
+            single_data[11]+=1
+    max = 0
+    for x in range (0, len(single_data)):
+        if single_data[x] > max:
+            max = single_data[x]
+    sign = signs_array[x]
+    matches = true
+    if sign != single_data[12]:
+        matches = false
+        total_data[0]+=1
+    total_data[1]+=1
+    percent_wrong = total_data[0]/total_data[1]
+    return render_template('results.html', matches=matches, predicted_sign=sign, actual_sign=single_data[12], percent_wrong=percent_wrong)
 
 @app.route('/birthday', methods=['POST'])
 def birthday():
@@ -47,10 +89,6 @@ def birthday():
         single_data.append('Sagittarius')
     return 'success'
 
-@app.route('/stats', methods=['GET'])
-def stats():
-
-    return 'f'
     #add info to all_data doc in data collection
     #do calculations
     #clear out current user doc
