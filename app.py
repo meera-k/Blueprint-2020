bc12f69c40cc1b9ff0c33d119299151a431838cf
meera-k <45214387+meera-k@users.noreply.github.com>
2020-02-09 10:52:58 -0500

    add home method

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ client = MongoClient("mongodb+srv://admin:admin@cluster0-e5pzb.mongodb.net/test?
 db = client['Blueprint-2020'] #database - only one necessary
 zodiac_key = db['zodiac_key'] #collection - perfect zodiac key
 data = db['data'] #collection - actual user data - will hold single and total session data
+page_num = 0
+
+@app.route('/', methods=['GET']) #specifies path and which methods it will work w
+def home():
+    return render_template('welcomepage.html', messages=messages.find({}), loggedIn = ('username' in session), username = session.get('username', '')) #returns html file sent to browser given the file and parameter for variables
+    #find returns all elements that fit a certain condition (eg. 'ts': [certain date] )
