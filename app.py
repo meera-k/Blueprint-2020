5d36764e5fa363287a72f96efc4445b51b54e043
meera-k <45214387+meera-k@users.noreply.github.com>
2020-02-09 09:49:52 -0500

    creating database and collections

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ from pymongo import MongoClient
 from flask import Flask, render_template, request, session
 import datetime
 
+app = Flask(__name__)
+client = MongoClient("mongodb+srv://admin:admin@cluster0-e5pzb.mongodb.net/test?retryWrites=true&w=majority")
+db = client['Blueprint-2020'] #database - only one necessary
+zodiac_key = db['zodiac_key'] #collection - perfect zodiac key
+data = db['data'] #collection - actual user data - will hold single and total session data
