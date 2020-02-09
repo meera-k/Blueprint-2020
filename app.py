2eaa20c5ff5c770a7641ac0048ba14fbd0a455a5
abriskin <45403282+abriskin@users.noreply.github.com>
2020-02-09 11:09:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,25 @@ db = client['Blueprint-2020'] #database - only one necessary
 zodiac_key = db['zodiac_key'] #collection - perfect zodiac key
 data = db['data'] #collection - actual user data - will hold single and total session data
 page_num = 0
+question_dict = {1: "",2: "",3: ""}
+answer_dict = {1: ["smart", "dumb"], 2: ["smart", "dumb"], 3: ["smart", "dumb"],}
 
 @app.route('/', methods=['GET']) #specifies path and which methods it will work w
-def home():
-    return render_template('welcomepage.html', messages=messages.find({}), loggedIn = ('username' in session), username = session.get('username', '')) #returns html file sent to browser given the file and parameter for variables
+def welcome_page():
+    return render_template('welcomepage.html') #returns html file sent to browser given the file and parameter for variables
     #find returns all elements that fit a certain condition (eg. 'ts': [certain date] )
+
+@app.route('/next', methods=['GET', 'POST'])
+def next():
+    page_num++
+    return render_template('questionpage.html', question_dict[page_num], answer_dict[page_num])
+
+@app.route('/birthday', methods=['POST'])
+def birthday():
+    #TODO make sure that info is passed in in datetime format
+
+@app.route('/stats', methods=['GET'])
+def stats():
+    #add info to all_data doc in data collection
+    #do calculations
+    #clear out current user doc
